729740ef714264fd4ebb9598e047f119fe380c11
rammasetty999 <ram.masetty1@gmail.com>
2024-08-07 19:58:26 +0530

    changedport

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router=require('./routes/vendorroute')
 const froutes=require('./routes/firmroute')
 const proutes=require('./routes/productroute')
 const app=express()
-const port=5000
+const port=process.env.port || 5000
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyparser.json())
 mongoose.connect(process.env.mongourl).then(()=>{
@@ -21,8 +21,6 @@ app.use('/uploads',express.static('uploads'))
 app.listen(port,()=>{
     console.log("server started succesfully")
 })
-// In your main server file
-app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    next();
-});
+app.use('/',(req,res)=>{
+    res.send("<h2>welcome to myapi")
+})
